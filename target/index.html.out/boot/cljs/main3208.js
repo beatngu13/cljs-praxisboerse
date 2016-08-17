3d274d0aea8ba73f5c37f2b5e0d88d085df65786
Daniel Kraus <daniel.kraus@mailbox.org>
2016-08-17 16:02:48 +0200

    Add .gitignore and rm files accordingly

--- target/index.html.out/boot/cljs/main3208.js
+++
@@ -1,4 +0,0 @@
-// Compiled by ClojureScript 1.9.93 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main3208');
-goog.require('cljs.core');
-goog.require('hoplon.app_pages._index_DOT_html');
