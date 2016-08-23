be08cd78b6d5d2d99aa162e49c0c8bf85d026698
Daniel Kraus <daniel.kraus@mailbox.org>
2016-08-23 08:09:32 +0200

    Use a regular select to display available offer types and countries

--- src/cljs_praxisboerse/core.cljs
+++ src/cljs_praxisboerse/core.cljs
@@ -12,23 +12,25 @@
 (def first-name (cell ""))
 (def offer-types (cell '()))
 (def countries (cell '()))
+(def offer-type-input (cell ""))
+(def country-input (cell "Weltweit"))
 
 (def invalid-iz? (cell= (nil? (re-matches #"^$|[a-z]{4}\d{4}" iz))))
 (def signed-in? (cell= (not= "" first-name)))
 
-(def base-url "https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST")
+(defn fetch-offers-by-type! []
+  (go (let [response (<! (http/get
+                           (str base-url "/joboffer/offers/" @offer-type-input "/0/-1")
+                           {:basic-auth {:username @iz :password @pw}}))]
+        (print (map :shortDescription (get-in response [:body :offers]))))))
 
 (defn fetch-offer-types! []
   (go (let [response (<! (http/get
                            (str base-url "/joboffer/offertypes/all")
                            {:basic-auth {:username @iz :password @pw}}))]
-        (swap! offer-types concat (map :name (:body response))))))
-
-(defn fetch-offers-by-type! [type]
-  (go (let [response (<! (http/get
-                           (str base-url "/joboffer/offers/" type "/0/-1")
-                           {:basic-auth {:username @iz :password @pw}}))]
-        (print (map :shortDescription (get-in response [:body :offers]))))))
+        (swap! offer-types concat (map #(select-keys % [:shortname :name]) (:body response)))
+        (swap! offer-type-input #(:shortname (first @offer-types)))
+        (fetch-offers-by-type!))))
 
 (defn sign-in! []
   (go (let [response (<! (http/get
