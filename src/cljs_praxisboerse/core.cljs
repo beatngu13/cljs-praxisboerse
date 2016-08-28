830fe5ad32f3b79b61fe1d7a0307257c9e094da8
Daniel Kraus <daniel.kraus@mailbox.org>
2016-08-28 11:42:19 +0200

    Sunday commit

--- src/cljs_praxisboerse/core.cljs
+++ src/cljs_praxisboerse/core.cljs
@@ -13,25 +13,26 @@
 (def offer-types (cell '()))
 (def countries (cell '()))
 (def offer-type-input (cell ""))
-(def country-input (cell "Weltweit"))
+(def country-input (cell ""))
+(def filter-input (cell ""))
 (def query-result (cell '()))
 
 (def invalid-iz? (cell= (nil? (re-matches #"^$|[a-z]{4}\d{4}" iz))))
 (def signed-in? (cell= (not= "" first-name)))
 
-(defn fetch-offers-by-type! []
+(defn fetch-offers! []
   (go (let [response (<! (http/get
                            (str base-url "/joboffer/offers/" @offer-type-input "/0/-1")
                            {:basic-auth {:username @iz :password @pw}}))]
-        (reset! countries (map :name (vals (get-in response [:body :countries]))))
+        (reset! countries (map #(select-keys % [:code :name]) (vals (get-in response [:body :countries]))))
         (reset! query-result (map :shortDescription (get-in response [:body :offers]))))))
 
 (defn fetch-offer-types! []
   (go (let [response (<! (http/get
                            (str base-url "/joboffer/offertypes/all")
                            {:basic-auth {:username @iz :password @pw}}))]
-        (swap! offer-types concat (map #(select-keys % [:shortname :name]) (:body response)))
-        (swap! offer-type-input #(:shortname (first @offer-types))))))
+        (reset! offer-types (map #(select-keys % [:shortname :name]) (:body response)))
+        (reset! offer-type-input (:shortname (first @offer-types))))))
 
 (defn sign-in! []
   (go (let [response (<! (http/get
@@ -44,5 +45,6 @@
 (add-watch signed-in? :fetch-offer-types (fn [_ _ _ new-state]
                                            (if (true? new-state) (fetch-offer-types!))))
 
+;; TODO Should watch country-input and filter-input as well.
 (add-watch offer-type-input :fetch-offers-by-type (fn [_ _ old-state new-state]
-                                                    (if (not= old-state new-state) (fetch-offers-by-type!))))
+                                                    (if (not= old-state new-state) (fetch-offers!))))
