6773871ab543ecc1e3603ca0247484e736127590
Daniel Kraus <daniel.kraus@mailbox.org>
2016-08-29 09:40:55 +0200

    Refactor

--- src/cljs_praxisboerse/templates.cljs
+++ src/cljs_praxisboerse/templates.cljs
@@ -1,30 +1,31 @@
 (ns cljs-praxisboerse.templates
-  ;; TODO Use refer to avoid prefix.
-  (:require [hoplon.core :as h]))
+  (:require [hoplon.core :refer [a br div h4 li small span]])
+  (:require-macros [hoplon.core :refer [text]]))
 
-(defn offer-tpl [offer company]
-  (h/li :class "list-group-item"
-    (h/span :id (str "company-" (:id offer)) :style "cursor: pointer;"
-      (h/h4
-        (h/text (:shortDescription offer))
-        (h/small (:companyName company) ", " (:city company))))
-    (h/div :style "display: inline;" :class "row"
-      (h/div :class "col-md-12" (h/text (:description offer)))
-      (h/div :class "col-md-12 well well-lg"
-        (h/div :class "row"
-          (h/div :class "col-md-3 col-xs-12"
-            ;; TODO Use h/text for consistency?
-            (h/h4 "Über das Unternehmen")
-            (:companyName company) (h/br)
-            (:street company) (h/br)
-            (:zipCode company) " " (:city company) (h/br)
-            (h/br)
-            (let [web (:website company)]
-              (h/a :href web :target "_blank" (h/text web))))
-          (h/div :class "col-md-3 col-xs-12"
-            ;; TODO Use h/text for consistency?
-            (h/h4 "Kontakt")
-            ;; TODO Use let for :contact and :email.
-            (:formOfAddress (:contact offer)) " " (:firstName (:contact offer)) " " (:secondName (:contact offer)) (h/br)
-            "Telefon: " (:telephone (:contact offer)) (h/br)
-            "E-Mail: " (h/a :href (str "mailto:" (:email (:contact offer))) (h/text (:email (:contact offer)))) (h/br)))))))
+(defn offer-list-item [offer company]
+  (let [web     (:website company)
+        contact (:contact offer)
+        mail    (:email contact)]
+    (li :class "list-group-item"
+      (span :id (str "company-" (:id offer)) :style "cursor: pointer;"
+        (h4
+          (text (:shortDescription offer))
+          (small (:companyName company) ", " (:city company))))
+      (div :style "display: none;" :class "row"
+        (div :class "col-md-12" (text (:description offer)))
+        (div :class "col-md-12 well well-lg"
+          (div :class "row"
+            (div :class "col-md-3 col-xs-12"
+              ;; TODO Use text for consistency?
+              (h4 "Über das Unternehmen")
+              (:companyName company) (br)
+              (:street company) (br)
+              (:zipCode company) " " (:city company) (br)
+              (br)
+              (a :href web :target "_blank" (text web)))
+            (div :class "col-md-3 col-xs-12"
+              ;; TODO Use text for consistency?
+              (h4 "Kontakt")
+              (:formOfAddress contact) " " (:firstName contact) " " (:secondName contact) (br)
+              "Telefon: " (:telephone contact) (br)
+              "E-Mail: " (a :href (str "mailto:" mail) (text mail)))))))))
