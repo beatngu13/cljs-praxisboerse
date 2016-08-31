0a6c2d0347779f25c96700a93f160c1e5bf1fd71
Daniel Kraus <daniel.kraus@mailbox.org>
2016-08-31 17:21:06 +0200

    Cells everywhere...

--- src/cljs_praxisboerse/templates.cljs
+++ src/cljs_praxisboerse/templates.cljs
@@ -1,18 +1,20 @@
 (ns cljs-praxisboerse.templates
   (:require [hoplon.core :refer [a br div h4 li small span]]
             [javelin.core :refer [cell]])
-  (:require-macros [hoplon.core :refer [text]]))
+  (:require-macros [hoplon.core :refer [text]]
+                   [javelin.core :refer [cell=]]))
 
+;; TODO Get rid off all those cells.
 (defn offer-list-item [offer company]
-  (let [web      (:website company)
-        contact  (:contact offer)
-        mail     (:email contact)
+  (let [web      (cell= (:website company))
+        contact  (cell= (:contact offer))
+        mail     (cell= (:email contact))
         details? (cell false)]
     (li :class "list-group-item"
       (span :style "cursor: pointer;" :click #(swap! details? not)
         (h4
           (text (:shortDescription offer))
-          (small (:companyName company) ", " (:city company))))
+          (small (cell= (:companyName company)) ", " (cell= (:city company)))))
       (div :class "row" :toggle details?
         (div :class "col-md-12" (text (:description offer)))
         (div :class "col-md-12 well"
@@ -20,14 +22,14 @@
             (div :class "col-md-5"
               ;; TODO Use text for consistency?
               (h4 "Über das Unternehmen")
-              (:companyName company) (br)
-              (:street company) (br)
-              (:zipCode company) " " (:city company) (br)
+              (cell= (:companyName company)) (br)
+              (cell= (:street company)) (br)
+              (cell= (:zipCode company)) " " (cell= (:city company)) (br)
               (br)
-              (a :href web :target "_blank" (text web)))
+              (cell= (a :href web :target "_blank" (text web))))
             (div :class "col-md-5"
               ;; TODO Use text for consistency?
               (h4 "Kontakt")
-              (:formOfAddress contact) " " (:firstName contact) " " (:secondName contact) (br)
-              "Telefon: " (:telephone contact) (br)
-              "E-Mail: " (a :href (str "mailto:" mail) (text mail)))))))))
+              (cell= (:formOfAddress contact)) " " (cell= (:firstName contact)) " " (cell= (:secondName contact)) (br)
+              "Telefon: " (cell= (:telephone contact)) (br)
+              "E-Mail: " (cell= (a :href (str "mailto:" mail) (text mail))))))))))
